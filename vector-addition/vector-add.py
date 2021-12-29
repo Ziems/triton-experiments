5b0d3a7ccdb60f57b28c125bddc904086beee447
Noah Ziems <nmziems@bsu.edu>
2021-12-29 14:32:45 -0500

    Fix line lengths

--- vector-addition/vector-add.py
+++ vector-addition/vector-add.py
@@ -14,20 +14,22 @@ def add_kernel(
     n_elements,# Size of the vector
     **meta,# Optional meta-parameters for the kernel # TODO ??
 ):
-    BLOCK_SIZE = meta['BLOCK_SIZE'] # How many inputs each program should process
+    # How many inputs each program should process
+    BLOCK_SIZE = meta['BLOCK_SIZE']
     # There are multiple 'program's processing different data.
     # We identify which program we are here
     pid = tl.program_id(axis=0) # 1d launch grid so axis is 0
     # This program will process inputs that are offset from the initial data.
-    # For instance, if you had a vector of length 256 and a block_size of 64, the programs
-    # would each access the lements [0:64, 64:128, 128:192, 192:256].
+    # For instance, if you had a vector of length 256 and a block_size of 64,
+    # the programs would each access the elements 
+    # [0:64, 64:128, 128:192, 192:256].
     # Note that offsets is a list of pointers
     block_start = pid * BLOCK_SIZE
     offsets = block_start + tl.arange(0, BLOCK_SIZE)
     # Create a mask to guard memory operations against out-of-bounds accesses
     mask = offsets < n_elements
-    # Load x and y from DRAM, masking out any extra elements in case the input is not
-    # a multiple of the block size 
+    # Load x and y from DRAM, masking out any extra elements in case the input 
+    # is not a multiple of the block size 
     x = tl.load(x_ptr + offsets, mask=mask)
     y = tl.load(y_ptr + offsets, mask=mask)
     output = x + y
@@ -39,17 +41,21 @@ def add(x: torch.Tensor, y: torch.Tensor):
     output = torch.empty_like(x)
     assert x.is_cuda and y.is_cuda and output.is_cuda
     n_elements = output.numel()
-    # The SPMD launch grid denotes the number of kernel instances that run in parallel.
-    # It is analagous to CUDA launch grids. It can be either Tuple[int] or Callable(metaparameters) -> Tuple[int]
+    # The SPMD launch grid denotes the number of kernel instances that run in 
+    # parallel.
+    # It is analagous to CUDA launch grids. 
+    # It can be either Tuple[int] or Callable(metaparameters) -> Tuple[int]
     # In this case, we use a 1D grid where the size is the number of blocks
-    grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']),) # This is odd to me.
+    grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']),)
     # NOTE:
-    # - Each torch.tensor object is implicitly converted into a pointer to its first element.
-    # - `triton.jit`'ed functions can be index with a launch grid to obtain a callable GPU kernel
+    # - Each torch.tensor object is implicitly converted into a pointer to its 
+    #   first element.
+    # - `triton.jit`'ed functions can be index with a launch grid to obtain a 
+    #   callable GPU kernel
     # - don't forget to pass meta-parameters as keywords arguments
     pgm = add_kernel[grid](x, y, output, n_elements, BLOCK_SIZE=1024)
-    # We return a handle to z but, since `torch.cuda.synchronize()` hasn't been called, the kernel is still
-    # running asynchronously at this point.
+    # We return a handle to z but, since `torch.cuda.synchronize()` hasn't been 
+    # called, the kernel is still running asynchronously at this point.
     return output
 
 torch.manual_seed(0)
@@ -77,7 +83,8 @@ print(
         line_names=['Triton', 'Torch'], # Label name for the lines
         styles=[('blue', '-'), ('green', '-')], # line styles
         ylabel='GB/s', # label name for the y-axis
-        plot_name='vector-add-performance', # name for the plot. Used also as ia file name for saving the plot.
+        # name for the plot. Used also as a file name for saving the plot.
+        plot_name='vector-add-performance',
         args={},
     )
 )
